7efa05dac5f4e5170bbcba0fcd351ddc262c69e8
belinets-web-dev <154679537+belinets-web-dev@users.noreply.github.com>
2024-01-28 20:30:25 +0600

    airpods

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const chooseColor = document.querySelectorAll('.choose-color__btn');
+const contentItem = document.querySelectorAll('.content-item');
+
+
+chooseColor.forEach(function(element){
+  element.addEventListener('click', open)
+})
+
+function open(evt){
+  const target = evt.currentTarget;
+  const button = target.dataset.button;
+  const contentActive = document.querySelectorAll(`.${button}`);
+
+  chooseColor.forEach(function(item){
+    item.classList.remove('choose-color__btn--activ')
+  });
+
+  target.classList.add('choose-color__btn--activ')
+
+  contentItem.forEach(function(item){
+    item.classList.remove('content-item__activ')
+  });
+
+  contentActive.forEach(function(item){
+    item.classList.add('content-item__activ')
+  });
+
+}
